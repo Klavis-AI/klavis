a8196996b00715dd0b7d75cc77f4c0275034fc1f
Uday Kakade <141299403+udaykakade25@users.noreply.github.com>
2025-07-21 19:54:27 +0530

    Create __init__.py in /tools

---
+++ mcp_servers/linkedin/tools/__init__.py
@@ -0,0 +1,28 @@
+from .auth import get_profile_info
+from .posts import create_text_post, create_article_post, get_user_posts
+from .network import get_network_updates
+from .search import search_people
+from .companies import get_company_info
+from .base import linkedin_token_context
+
+__all__ = [
+    # Auth/Profile
+    "get_profile_info",
+    
+    # Posts
+    "create_text_post",
+    "create_article_post", 
+    "get_user_posts",
+    
+    # Network
+    "get_network_updates",
+    
+    # Search
+    "search_people",
+    
+    # Companies
+    "get_company_info",
+    
+    # Base
+    "linkedin_token_context",
+]
