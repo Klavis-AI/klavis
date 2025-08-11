bc530a88646a78dcc76be4da4989ef5a095c0e30
Prabhu Avula <pavula1@hawk.iit.edu>
2025-08-11 01:05:31 -0600

    replicate-multi-tool: comprehensive README, add upscale_image tool, update Dockerfile, troubleshooting + helpful links, export upscale tool

--- mcp_servers/replicate-multi-tool/tools/__init__.py
+++ mcp_servers/replicate-multi-tool/tools/__init__.py
@@ -10,6 +10,7 @@ from .generate_image import generate_image
 from .serpapi_search import search_web_query
 from .tavily_search import search_with_tavily, summarize_webpage
 from .elevenlabs_voice import generate_voice_from_text
+from .upscale_image import upscale_image
 
 __all__ = [
     "generate_image",
@@ -17,6 +18,7 @@ __all__ = [
     "search_with_tavily",
     "summarize_webpage",
     "generate_voice_from_text",
+    "upscale_image",
 ]
 
 
