49180f74dd110dc4279d8bacf6e90a5d2d7b0422
ahmad-ali <ahmadabdelaziz8@gmail.com>
2025-09-25 14:46:00 +0300

    feat(reddit_search): add create_post to module exports

--- mcp_servers/reddit_search/tools/__init__.py
+++ mcp_servers/reddit_search/tools/__init__.py
@@ -3,6 +3,7 @@ from .find_subreddits import find_relevant_subreddits
 from .search_posts import search_subreddit_posts
 from .get_comments import get_post_and_top_comments
 from .find_similar import find_similar_posts_reddit
+from .create_post import create_post
 from .base import SubredditInfo, PostInfo, CommentInfo, PostDetails
 
 __all__ = [
@@ -10,6 +11,7 @@ __all__ = [
     "search_subreddit_posts", 
     "get_post_and_top_comments",
     "find_similar_posts_reddit",
+    "create_post",
     "SubredditInfo",
     "PostInfo", 
     "CommentInfo",
