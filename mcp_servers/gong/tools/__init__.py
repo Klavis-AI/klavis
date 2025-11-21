2a664189508913759eadffe639b1e45b193f6f9d
Zihao Lin <zihaolin@klavis.ai>
2025-11-20 23:23:05 -0800

    revamp Gong tools (#709)

--- mcp_servers/gong/tools/__init__.py
+++ mcp_servers/gong/tools/__init__.py
@@ -1,8 +1,9 @@
 from .base import access_key_context, access_key_secret_context, extract_credentials
 from .transcripts import get_transcripts_by_user
 from .extensive import get_extensive_data
-from .calls import list_calls, add_new_call
+from .calls import list_calls, add_new_call, get_call_by_id
 from .transcripts import get_call_transcripts
+from .users import list_all_users, get_user_by_id, get_user_settings_history, list_users_by_filter
 
 __all__ = [
     "access_key_context",
@@ -13,4 +14,9 @@ __all__ = [
     "get_extensive_data",
     "list_calls",
     "add_new_call",
+    "get_call_by_id",
+    "list_all_users",
+    "get_user_by_id",
+    "get_user_settings_history",
+    "list_users_by_filter",
 ] 
