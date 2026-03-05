24b2497b53071f6ad658770ab58acbe0ccce3dc0
lliuzzheng <lliuzzheng@gmail.com>
2026-03-05 11:45:55 +0800

    Use correct version of scholarly

--- mcp_servers/scholarly/src/mcp_scholarly/__init__.py
+++ mcp_servers/scholarly/src/mcp_scholarly/__init__.py
@@ -1,4 +1,8 @@
-from .server import main
+from . import server
+
+def main():
+    """Main entry point for the package."""
+    server.main()
 
 # Optionally expose other important items at package level
-__all__ = ['main']
+__all__ = ['main', 'server']
